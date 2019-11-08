69c43d7598aaf55b97346ca1348b99812b185d46
Valerie <vvlovelera@mail.ru>
2019-11-08 15:21:59 +0300

    Update Source.cpp

---
+++ Farm/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//тут мы будем должны применить композицию, абстрактную фабрику.
+//на следующем шаге опреелить возможніе добавления паттернов при существующем функционале
+//добавить функционал
+class World {
+
+};
+class Sheep{
+
+};
+class Cell{};
+class Obstacle{
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	system("pause");
+}
