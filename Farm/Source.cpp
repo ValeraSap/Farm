bca885df8ecbea1f6d014fab3a88618a3ca9c587
Valerie <vvlovelera@mail.ru>
2019-11-10 22:56:17 +0300

    Новые классы

--- Farm/Source.cpp
+++ Farm/Source.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+//#include <thread>
 using namespace std;
+//use threads
+//patterns:abstract factory, strategy, посредник,
 //тут мы будем должны применить композицию, абстрактную фабрику.
 //на следующем шаге опреелить возможніе добавления паттернов при существующем функционале
 //добавить функционал
+enum Direction {North, South, East, West};
+class MapSite {
+public:
+  virtual void Enter() = 0;
+};
 class World {
+//Cells is vector
+public:
+  World(int n, int m){
 
+  }
 };
 class Sheep{
 
 };
-class Cell{};
-class Obstacle{
+class Cell{
+bool isBald;
+public:
+  Cell(){
+    isBald=false;
+  }
+  bool isBald(){return isBald;}
+/*  void getEated(){
+    isBald=true;
+  }*/
+};
+class Tree{
 
 };
+class Stone{};
+/*class Obstacle{};*/
 
 int main()
 {
